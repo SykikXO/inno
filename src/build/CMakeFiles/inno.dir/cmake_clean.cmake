a21df06338d97a62c390085e57cc1cbd2303e21a
sykikxo <xo.sykik@gmail.com>
2026-01-02 01:39:18 +0530

    Daemonize by default

---
+++ src/build/CMakeFiles/inno.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inno.dir/link.d"
+  "CMakeFiles/inno.dir/generated/wlr-layer-shell-unstable-v1-protocol.c.o"
+  "CMakeFiles/inno.dir/generated/wlr-layer-shell-unstable-v1-protocol.c.o.d"
+  "CMakeFiles/inno.dir/generated/xdg-shell-protocol.c.o"
+  "CMakeFiles/inno.dir/generated/xdg-shell-protocol.c.o.d"
+  "CMakeFiles/inno.dir/handler/dbus_handler.c.o"
+  "CMakeFiles/inno.dir/handler/dbus_handler.c.o.d"
+  "CMakeFiles/inno.dir/layer/cairo_text.c.o"
+  "CMakeFiles/inno.dir/layer/cairo_text.c.o.d"
+  "CMakeFiles/inno.dir/layer/layerhandler.c.o"
+  "CMakeFiles/inno.dir/layer/layerhandler.c.o.d"
+  "CMakeFiles/inno.dir/layer/render.c.o"
+  "CMakeFiles/inno.dir/layer/render.c.o.d"
+  "CMakeFiles/inno.dir/layer/wayland_init.c.o"
+  "CMakeFiles/inno.dir/layer/wayland_init.c.o.d"
+  "CMakeFiles/inno.dir/src/config.c.o"
+  "CMakeFiles/inno.dir/src/config.c.o.d"
+  "CMakeFiles/inno.dir/src/main.c.o"
+  "CMakeFiles/inno.dir/src/main.c.o.d"
+  "generated/wlr-layer-shell-unstable-v1-client-protocol.h"
+  "generated/wlr-layer-shell-unstable-v1-protocol.c"
+  "generated/xdg-shell-client-protocol.h"
+  "generated/xdg-shell-protocol.c"
+  "inno"
+  "inno.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/inno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
